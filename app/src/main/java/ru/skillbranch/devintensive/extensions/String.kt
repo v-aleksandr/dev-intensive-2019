8d6d376afca8962dd7f091d5f1acd514fda52e67
v-aleksandr <voskolup.aleksandr@gmail.com>
2019-07-10 10:00:26 +0300

    hometask_2 added String.stripHtml()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,3 +4,13 @@ fun String.truncate(count: Int = 16): String {
     val line = this.trim()
     return if (line.length<=count) line else line.substring(0,count).trim()+"..."
 }
+fun String.stripHtml(): String {
+//    s/<(.*?)>//g
+    var result =this.replace("&nbsp;"," ")
+    val noTags = """<[^>]*>""".toRegex()
+    val noSpaces = """\s+""".toRegex()
+    val noEscapes = """&amp;|&gt;|&lt;""".toRegex()
+    result= noTags.replace(result," ")
+    result= noEscapes.replace(result,"")
+    return noSpaces.replace(result," ").trim()
+}
